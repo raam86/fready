a3263bf07a7510168dfc79d51db4970bc80940c9
Raam Rosh-Hai <raam86@gmail.com>
2014-08-11 11:04:03 +0300

    Update fready.js
    Took example out of code

--- fready.js
+++ fready.js
@@ -6,17 +6,3 @@ $.fn.fready = function() {
 	});
 	return this;
 }
-var arr = [];
-function dude (argument) {
-	console.log("foofl");
-}
-function dude1 (argument) {
-	console.log("foofll");
-}
-function dude2 (argument) {
-	console.log("foolllf");
-}
-arr.push(dude);
-arr.push(dude1);
-arr.push(dude2);
-$(document).fready(arr)
